71b1f8efe5f40cda5131c4aa4203cb873c1421b9
Jonathan Bruce <supimjbruce@gmail.com>
2024-10-21 23:22:38 -0400

    Updated embolden_home.js
    Spent a little bit of time working on the code of this Home screen. I made styles for the Title and Subtext and also tried to figure out to flex the ".j." to the bottom of the app.
    
    I'm going to revisit this home screen in a couple of tutorials once I work through "react-native-navigation" and also get a better feel for how I want this app to be.
    
    Thanks for reading! Goodnight 😴 
    .j.

--- embolden_home.js
+++ embolden_home.js
@@ -1,51 +1,54 @@
 import React from 'react';
-import {View, Scrollview, Text, TextInput, Stylesheet} from 'react-native';
-import emboldenLoginPage from './embolden_login';
+import {View, ScrollView, Text, Button, TextInput, StyleSheet} from 'react-native';
+/*import emboldenLoginPage from '/Users/jonathanbruce/Coding/EmboldenApp/embolden_login.js';*/
 
 const emboldenHomePage = () => {
 
     return (
-        <Scrollview>
+        <ScrollView>
             <View style={styles.container}>
-                <Text>Embolden</Text>
-                <Text>An app to empower and encourage those on mission for Christ</Text>
-                <></> //fragement or line break right here
+                <Text style={styles.title}>Embolden</Text>
+                <Text style={styles.subtext}>An app to empower and encourage those on mission for Christ</Text>
+                  <Button
+                        style={styles.buttonPadding}
+                        title="         "
+                  />
                 <Text>By JBruce</Text>
-
             </View>
             <View>
-                <emboldenLoginPage></emboldenLoginPage> //page where the Login View lives
             </View>
             <Text style={styles.footer}>.j.</Text>
-        </Scrollview>
-            /*<View>
-                <Text>Login</Text>
-                <TextInput
-                    style={{height: 40}}
-                    placeholder="Username"
-                    defaultValue=""
-                />
-                <TextInput
-                    style={{height: 40}}
-                    placeholder="Password"
-                    defaultValue=""
-                />
-                <></>
-                <Text>Don't have an Embolden account? Tap the button below!</Text>
-                <Button
-                    title="Create Account"
-                    color="dodgerblue"
-                />
-            </View>*/
+        </ScrollView>
+          /*<View>
+              <Text>Login</Text>
+              <TextInput
+                  style={{height: 40}}
+                  placeholder="Username"
+                  defaultValue=""
+              />
+              <TextInput
+                  style={{height: 40}}
+                  placeholder="Password"
+                  defaultValue=""
+              />
+              <></>
+              <Text>Don't have an Embolden account? Tap the button below!</Text>
+              <Button
+                  title="Create Account"
+                  color="dodgerblue"
+              />
+          </View>*/
 )};
 
 /*
 Things that I need to do:
 1. Check if how I am linking to the other View (the login page) is correct and
 that is the right way to do it!
+2. Learn the React Native Navigation from Wix and React Native tutorial so I can
+give this multiple screens!
 */
 
-const styles = Stylesheet.create ({
+const styles = StyleSheet.create ({
     container: {
         flex: 1,
         justifyContent: "center",
@@ -53,8 +56,25 @@ const styles = Stylesheet.create ({
         backgroundColor: "gold",
     },
 
+    title: {
+        fontSize: 64,
+        fontWeight: 'bold',
+    },
+
+    subtext: {
+        fontSize: 16,
+        marginLeft: 40,
+        marginRight: 40,
+
+    },
+
+    buttonPadding: {
+        width: 15,
+    },
+
     footer: {
-        alignItems: "bottom",
+        alignSelf: 'flex-end', /* Need to figure out how to make a footer and also
+        put this at the end of the app.*/
     },
 
 });
