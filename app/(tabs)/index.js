cc2202c324a1aa82a619d324d1436cd9531d02e5
Jonathan Bruce <supimjbruce@gmail.com>
2024-11-20 21:19:46 -0500

    Updated index.js
    Hello GitHub! I realized I never uploaded the new code of the index page, where it has my old Embolden_Home code placed in it. Tonight I updated this page to have and test the BoldButton.
    
    Thank you for reading!
    .j.

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,33 +1,92 @@
-import {Text, View, StyleSheet} from "react-native";
-import {Link} from 'expo-router';
+import {View, ScrollView, Text, Button, TextInput, StyleSheet} from "react-native";
+/* import {Link} from 'expo-router'; */
+import React from 'react';
+import BoldButton from "../../components/boldbutton";
+/* import EmboldenLoginPage from '/Users/jonathanbruce/Coding/FirstExpoProject/app/embolden_login.js';
+import BoldButton from '/Users/jonathanbruce/Coding/FirstExpoProject/app/boldbutton.js'; */
+
+/* ^ Make the Login page and BoldButton as Modals/Components and put them in the /Components folder! */
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Embolden</Text>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to the About screen!
-      </Link>
+    <View>
+      <ScrollView>
+          <View style={styles.container}>
+            <Text style={styles.title}>Embolden</Text>
+            <Text style={styles.subtext}>An app to empower and encourage those on mission for Christ</Text>
+            <Button
+                    style={styles.buttonPadding}
+                    title="         "
+            />
+            <Text>By JBruce</Text>
+          </View>
+          <View>
+            <BoldButton></BoldButton>
+          </View>
+          <Text style={styles.footer}>.j.</Text>
+      <View>
+      </View>
+      </ScrollView>
     </View>
+
+    /*<View>
+      <Text>Login</Text>
+      <TextInput
+          style={{height: 40}}
+          placeholder="Username"
+          defaultValue=""
+      />
+      <TextInput
+          style={{height: 40}}
+          placeholder="Password"
+          defaultValue=""
+      />
+      <></>
+      <Text>Don't have an Embolden account? Tap the button below!</Text>
+      <Button
+          title="Create Account"
+          color="dodgerblue"
+      />
+    </View>*/
   );
 }
 
-const styles = StyleSheet.create({
+/*
+Things that I need to do:
+1. Check if how I am linking to the other View (the login page) is correct and
+that is the right way to do it!
+2. Learn the React Native Navigation from Wix and React Native tutorial so I can
+give this multiple screens!
+*/
+
+const styles = StyleSheet.create ({
   container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: "center",
-    alignItems: "center",
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "gold",
+      paddingTop: 25,
+      paddingBottom: 25,
   },
 
-  text: {
-    color: '#fff',
+  title: {
+      fontSize: 64,
+      fontWeight: 'bold',
   },
 
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: 'fff',
+  subtext: {
+      fontSize: 16,
+      marginLeft: 40,
+      marginRight: 40,
   },
+
+  buttonPadding: {
+      width: 15,
+  },
+
+  footer: {
+      alignSelf: 'flex-end', /* Need to figure out how to make a footer and also
+      put this at the end of the app.*/
+  },
+
 });
