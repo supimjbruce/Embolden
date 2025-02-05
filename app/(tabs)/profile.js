a35ceebfc4d4ff1d836775fa05269dc330ef093e
Jonathan Bruce <supimjbruce@gmail.com>
2025-02-05 10:08:01 -0500

    Updated profile.js
    There were a lot of errors from when I made this page; from uncommented comments to missing imports and the like. Just edited it so it's at least operational now.
    
    Thanks for reading!
    .j.

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,29 +1,30 @@
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Button, Image} from 'react-native';
+import React from 'react';
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Profile Screen</Text>
       <View>
-        <Image></Image> /* Profile photo */
+        <Image></Image> {/* Profile photo */}
       </View>
       <View>
-        <Text>Username</Text> /* Username */
+        <Text>Username</Text> {/* Username */}
       </View>
-      <View> /*Profile Details Container*/
+      <View> {/*Profile Details Container*/}
         <View>
-          <Text>Current Mission</Text> /*Current mission*/
+          <Text>Current Mission</Text> {/*Current mission*/}
           <Text>Description</Text>
         </View>
         <View>
-          <Text>Past Missions</Text> /*Past missions*/
+          <Text>Past Missions</Text> {/*Past missions*/}
         </View>
         <View>
-          <Text>About Me</Text> /*About me section*/
+          <Text>About Me</Text> {/*About me section*/}
           <Text>Bio</Text>
         </View>
-        <Button>Friends List</Button> /*Friends List Button*/
-        <Button>Update Profile</Button> /*Update Profile Button*/
+        <Button>Friends List</Button> {/*Friends List Button*/}
+        <Button>Update Profile</Button> {/*Update Profile Button*/}
       </View>
     </View>
   );
