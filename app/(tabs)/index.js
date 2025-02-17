8ffcbb948d811f5f485da09b3f8af5eea7241234
Jonathan Bruce <supimjbruce@gmail.com>
2025-02-16 22:59:46 -0500

    Updated index.js
    Did a good bit of work on this one! Added in state so I could make the new login modal appear and disappear with a button (still need to fine tune this) as well as restyled the actual content and styling of the title, subtitle and UI layout of this page.
    
    Liked working on this tonight!
    .j.

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,31 +1,46 @@
 import {View, ScrollView, Text, Button, TextInput, StyleSheet} from "react-native";
 /* import {Link} from 'expo-router'; */
-import React from 'react';
+import React, {useState} from 'react';
 import BoldButton from "../../components/boldbutton";
+import LoginModal from "../../components/loginModal";
 /* import EmboldenLoginPage from '/Users/jonathanbruce/Coding/FirstExpoProject/app/embolden_login.js';
 import BoldButton from '/Users/jonathanbruce/Coding/FirstExpoProject/app/boldbutton.js'; */
 
 /* ^ Make the Login page and BoldButton as Modals/Components and put them in the /Components folder! */
 
-export default function Index() {
+const LoginModalReference = () => {
   return (
     <View>
+      <LoginModal></LoginModal>
+    </View>
+  );
+};
+
+export default function Index() {
+  const [isLoginModalVisible, setLoginModalVisible] = useState(false);
+
+  const handleLoginButtonPress = () => {
+    setLoginModalVisible(!isLoginModalVisible);
+  };
+
+  return (
+    <View style={styles.container}>
       <ScrollView>
-          <View style={styles.container}>
-            <Text style={styles.title}>Embolden</Text>
-            <Text style={styles.subtext}>An app to empower and encourage those on mission for Christ</Text>
-            <Button
-                    style={styles.buttonPadding}
-                    title="         "
-            />
-            <Text>By JBruce</Text>
+          <View style={styles.indexContent}> {/* Get all of this content centered like 'Ofe'*/}
+            <Text style={styles.title}>EMBOLDEN</Text>
+            <Text style={styles.subtext}>An app to empower and encourage <br></br>those on mission for Christ</Text>
+
+            <View>
+              <BoldButton></BoldButton>
+            </View>
+
+            <View>
+              <Button title="Login" onPress={handleLoginButtonPress} />
+              {isLoginModalVisible && <LoginModalReference />}
+            </View>
+
+            <Text style={styles.footer}><b>.j.</b></Text>
           </View>
-          <View>
-            <BoldButton></BoldButton>
-          </View>
-          <Text style={styles.footer}>.j.</Text>
-      <View>
-      </View>
       </ScrollView>
     </View>
 
@@ -62,22 +77,27 @@ give this multiple screens!
 const styles = StyleSheet.create ({
   container: {
       flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "gold",
+      backgroundColor: "#FFC94A",
       paddingTop: 25,
       paddingBottom: 25,
   },
 
+  indexContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   title: {
       fontSize: 64,
-      fontWeight: 'bold',
+      fontWeight: '900',
   },
 
   subtext: {
       fontSize: 16,
       marginLeft: 40,
       marginRight: 40,
+      textAlign: 'center',
   },
 
   buttonPadding: {
