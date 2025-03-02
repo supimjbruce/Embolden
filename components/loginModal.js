8f1313ea9980ed59b70c2c6139f7b2b6ad968e1d
Jonathan Bruce <supimjbruce@gmail.com>
2025-03-02 10:25:23 -0500

    Updated loginModal.js
    Spent some time yesterday and today changing the loginModal to be like the trackr overlay modal so it doesn't spawn in the code and made the styling all weird.
    
    I like how this is looking and now I just need to add some functionality to it.
    
    Happy Jesus Day
    .j.

--- components/loginModal.js
+++ components/loginModal.js
@@ -1,40 +1,123 @@
-import React from 'react';
-import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Modal, TextInput, Pressable} from 'react-native';
 
-export default function LoginModal() {
+const LoginModal = () => {
+
+const [isModalVisible, setModalVisible] = useState();
+
     return (
-        <View style={styles.container}>
-            <TextInput
+      <View style={styles.container}> {/* Thank you Google once again for help with this code :) */}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalTitle}>Log In</Text>
+              <TextInput
                 style={styles.textInput}
                 placeholder="  Username"
                 defaultValue=""
-            />
-            <TextInput
-                style={styles.textInput}
+              />
+              <br></br>
+              <TextInput
+                style={[styles.textInput, {marginTop: 0}]}
                 placeholder="  Password"
                 defaultValue=""
-            />
-            <></>
-            <Text style={{textAlign: 'center', padding: 10,}}>Don't have an Embolden account? Tap the button below!</Text>
-            <Button
-                title="Create Account"
-                color="dodgerblue"
-            />
-        </View>
+              />
+              <br></br>
+              <Text style={{textAlign: 'center', padding: 10,}}>Don't have an Embolden account? Tap the button below!</Text>
+              <View style={styles.buttonAlign}>
+                <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
+                  <Text style={styles.closeButtonText}>Create Account</Text>
+                </Pressable>
+              </View>
+              <br></br>
+              <View style={styles.buttonAlign}> {/* I would like this button to be aligned top right with an X*/}
+                <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
+                  <Text style={styles.closeButtonText}>Close</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#798645',
-        padding: 25,
-        marginTop: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    },
+    
+    modalContent: {
+      maxHeight: '60%',
+      maxWidth: '85%',
+      minHeight: '30%',
+      minWidth: '30%',
+      backgroundColor: '#FFCC70',
+      padding: 20,
+      borderRadius: 20,
+      justifyContent: 'center',
+    },
+
+    modalTitle: {
+      color: '#FFFADD',
+      fontSize: 64,
+      fontWeight: '800',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+    },
+
+    modalParagraphTitle: {
+      fontFamily: 'Poppins-Medium',
+      fontSize: 16,
+      textAlign: 'center',
+      padding: 10,
     },
 
     textInput: {
-        backgroundColor: 'white',
-        height: 40,
-        borderRadius: 15,
-        margin: 10,
+      borderWidth: 1,
+      borderColor: 'black',
+      borderRadius: 5,
+      marginTop: 10,
     },
-});
+
+    buttonAlign: {
+      alignItems: 'center',
+    },
+
+    closeButton: {
+      height: 50,
+      width: 150,
+      maxHeight: 100,
+      maxWidth: 300,
+      backgroundColor: '#8ECDDD',
+      borderRadius: 23,
+      paddingTop: 15, /* Padding is the space inside of the element :) */
+      paddingBottom: 15,
+      paddingLeft: 5,
+      margin: 10, /* Space OUTSIDE of the element */
+      textAlign: 'center',
+    },
+
+    closeButtonText: {
+      fontSize: 16,
+      textAlign: 'center',
+    },
+
+  });
+
+export default LoginModal;
