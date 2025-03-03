4b36ce60e78ef621374a81f6603b977887a1a1c1
Jonathan Bruce <supimjbruce@gmail.com>
2025-03-02 23:42:01 -0500

    Updated loginModal.js
    Spent some time tonight before bed working on the loginModal.js and I like where it is now! Got the close button up top and to the right, changed some colors and fonts, got some comments out. Had a good time coding
    
    Goodnight GitHub
    .j.

--- components/loginModal.js
+++ components/loginModal.js
@@ -15,6 +15,12 @@ const [isModalVisible, setModalVisible] = useState();
         >
           <View style={styles.modalContainer}>
             <View style={styles.modalContent}>
+              <View style={styles.topFlexButton}>
+                <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
+                  <Text style={styles.closeButtonText}>X</Text>
+                </Pressable>
+              </View>
+
               <Text style={styles.modalTitle}>Log In</Text>
               <TextInput
                 style={styles.textInput}
@@ -27,17 +33,11 @@ const [isModalVisible, setModalVisible] = useState();
                 placeholder="  Password"
                 defaultValue=""
               />
-              <br></br>
+              <br></br> {/* I need to put normal Poppins on this v */}
               <Text style={{textAlign: 'center', padding: 10,}}>Don't have an Embolden account? Tap the button below!</Text>
               <View style={styles.buttonAlign}>
-                <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
-                  <Text style={styles.closeButtonText}>Create Account</Text>
-                </Pressable>
-              </View>
-              <br></br>
-              <View style={styles.buttonAlign}> {/* I would like this button to be aligned top right with an X*/}
-                <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
-                  <Text style={styles.closeButtonText}>Close</Text>
+                <Pressable style={styles.createAccountButton} onPress={() => setModalVisible(false)}>
+                  <Text style={styles.createAccountText}>Create Account</Text>
                 </Pressable>
               </View>
             </View>
@@ -60,20 +60,26 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: 'rgba(0, 0, 0, 0.6)',
     },
+
+    topFlexButton: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'flex-end',
+    },
     
     modalContent: {
       maxHeight: '60%',
       maxWidth: '85%',
       minHeight: '30%',
       minWidth: '30%',
-      backgroundColor: '#FFCC70',
+      backgroundColor: "#FFC94A",
       padding: 20,
       borderRadius: 20,
       justifyContent: 'center',
     },
 
     modalTitle: {
-      color: '#FFFADD',
+      color: '#FFFADD', /* Work on these colors */
       fontSize: 64,
       fontWeight: '800',
       justifyContent: 'center',
@@ -102,22 +108,37 @@ const styles = StyleSheet.create({
     closeButton: {
       height: 50,
       width: 150,
+      maxHeight: 50,
+      maxWidth: 50,
+      backgroundColor: '#8ECDDD',
+      borderRadius: 13,
+      margin: 10, /* Space OUTSIDE of the element */
+      justifyContent: 'center',
+    },
+
+    closeButtonText: {
+      fontFamily: 'Poppins-Black',
+      fontSize: 18,
+      textAlign: 'center',
+    },
+
+    createAccountButton: {
+      height: 50,
+      width: 150,
       maxHeight: 100,
       maxWidth: 300,
       backgroundColor: '#8ECDDD',
       borderRadius: 23,
-      paddingTop: 15, /* Padding is the space inside of the element :) */
-      paddingBottom: 15,
-      paddingLeft: 5,
       margin: 10, /* Space OUTSIDE of the element */
       textAlign: 'center',
+      justifyContent: 'center',
     },
 
-    closeButtonText: {
-      fontSize: 16,
+    createAccountText: {
+      fontFamily: 'Poppins-Medium',
+      fontSize: 14,
       textAlign: 'center',
     },
-
   });
 
 export default LoginModal;
